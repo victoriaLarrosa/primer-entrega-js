e91e4964ed3a0ebb044d82df1b420fea4bd94d1e
victoriaLarrosa <rominalarrosa1999@gmail.com>
2022-10-10 23:20:14 -0300

    segunda entrega

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function carritoFinal() {
+/*function carritoFinal() {
       alert("Gracias por la compra")
       carrito.forEach((carritoFinal) => {
          console.log("Producto: " + carritoFinal.nombre + " , unidades: " + carritoFinal.unidades + ", total a pagar por producto " + carritoFinal.unidades * carritoFinal.precio)
@@ -31,19 +31,118 @@ function validarSeleccion() {
         validarSeleccion()
     }
 }
+*/
+const shopContent = document.getElementById("shopContent")
+const verCarito = document.getElementById("verCarrito")
+const modalContainer = document.getElementById("modal-container")
+
 
 const productos = [
-    {nombre: "stella", precio: 320},
-    {nombre: "zillertal", precio: 300},
-    {nombre: "zillertal ipa", precio: 300},
-    {nombre: "zillertal apa", precio: 300},
-    {nombre: "patricia", precio: 280},
+    {
+    id:1,
+    nombre: "stella",
+    precio: 320,
+    img: "./assets/stella.jpg"},
+
+    {
+    id:2,
+    nombre: "zillertal",
+    precio: 300,
+    img: "./assets/zillertal.jpg"},
+
+    {
+    id:3,
+    nombre: "zillertal ipa",
+    precio: 320,
+    img: "./assets/zillertal-ipa.png"},
+
+    {
+    id:4,
+    nombre: "zillertal apa",
+    precio: 320,
+    img: "./assets/zillertal-apa.jpg"},
+
+    {
+    id:5,
+    nombre: "patricia",
+    precio: 280,
+    img: "./assets/lager.png",
+    },
 
 ];
 
 let carrito = []
 
-let tienda = prompt("Bienvenido a TodoAca, Usted esta ubicado en la seccion de CERVEZAS, Ingrese 'si' en caso de querer continuar, o 'no' en caso de querer cancelar la compra");
+productos.forEach ((product) => {
+    let content = document.createElement ("div");
+    content.className = "card",
+    content.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p class="price">$${product.precio}</p>`;
+
+    shopContent.append(content);
+
+    let comprar = document.createElement("button")
+    comprar.className = "comprar";
+    comprar.innerText = "comprar";
+
+    content.append(comprar);
+
+    comprar.addEventListener("click", () =>{
+        carrito.push ({
+            id: product.id,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio,
+        });
+        console.log (carrito);
+    }); 
+});
+
+verCarrito.addEventListener("click", () => {
+    modalContainer.innerHTML = ""
+    modalContainer.style.display = "flex"
+    const modalHeader = document.createElement("div")
+    modalHeader.className = "modal-header"
+    modalHeader.innerHTML = `
+    <h1 class="modal-header-title">Carrito</h1>
+    `;
+    modalContainer.append(modalHeader);
+    
+    const modalButton = document.createElement("h1");
+    modalButton.innerHTML = "X";
+    modalButton.className = "modal-header-button";
+
+    modalButton.addEventListener("click", () => {
+        modalContainer.style.display = "none"
+    })
+
+    modalHeader.append(modalButton);
+
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement("div")
+        carritoContent.className = "modal-content"
+        carritoContent.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p>$${product.precio}</p>
+    `;
+    modalContainer.append(carritoContent);
+    });
+
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+
+    const compraTotal = document.createElement("div")
+    compraTotal.className = "compra-total"
+    compraTotal.innerHTML = `El total a pagar es de: $${total}`;
+    modalContainer.append(compraTotal)
+
+});
+
+
+
+/*let tienda = prompt("Bienvenido a TodoAca, Usted esta ubicado en la seccion de CERVEZAS, Ingrese 'si' en caso de querer continuar, o 'no' en caso de querer cancelar la compra");
 
 while(tienda != "si" && tienda != "no"){
     alert("Porfavor, Ingrese si o  no")
@@ -51,7 +150,7 @@ while(tienda != "si" && tienda != "no"){
 }
 
 if (tienda === "si") {
-    alert("a continuacion le mostraremos nuestra variedad de cervezas para que pueda elegir")
+    ("a continuacion le mostraremos nuestra variedad de cervezas para que pueda elegir")
 
     seleccion = tienda
 
@@ -100,6 +199,7 @@ if (tienda === "si") {
     alert("Gracias por visitarnos, hasta pronto!")
 
 }
+*/
 
 
 
